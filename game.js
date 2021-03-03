d3f68695ef7858f282d4b82b0f04841bcb3cc5de
tothgeza <toth.geza.0425@gmail.com>
2021-03-03 12:09:29 +0100

    Draw table

--- game.js
+++ game.js
@@ -1,7 +1,30 @@
 initGame();
 
 function initGame() {
-
+    startGame();
     // Your game can start here, but define separate functions, don't write everything in here :)
 
+}
+function initLeftClick() {
+    let cells = document.querySelectorAll("col");
+    for (let cell of cells) {
+        cell.addEventListener('click', leftClick(event))
+    }
+}
+function leftClick(event) {
+    if (event.currentTarget.classList.contains("target-bg")) {
+        console.log("Találat");
+    }
+}
+
+function startGame(){
+    let cells = document.getElementsByClassName("col");
+    console.log(cells)
+    const interval = setInterval(function(){
+        let randomNum = (Math.random() * cells.length) | 0;
+        console.log(randomNum);
+        cells[randomNum].classList.toggle("target-bg");
+        // start timer
+        setTimeout(() => {cells[randomNum].classList.toggle("target-bg"); }, 500)
+    },3500 )
 }
