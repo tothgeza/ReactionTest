a8b70bf6dc46304229342792ddd68100b78eab70
tothgeza <toth.geza.0425@gmail.com>
2021-03-04 06:59:31 +0100

    Make results

--- game.js
+++ game.js
@@ -19,8 +19,11 @@ function initLeftClick() {
 
 function leftClick(event) {
     if (event.currentTarget.classList.contains("target")) {
-        console.log("True");
-        score.round1.score= event.currentTarget.dataset.round;
+        let roundCurrent = "Round" + event.currentTarget.dataset.round.toString(),
+            clickTime = Date.now(),
+            showTime = sessionStorage.getItem("showTime");
+        console.log(clickTime - showTime)
+        localStorage.setItem(roundCurrent, clickTime - showTime);
     } else {
         console.log("False");
     }
@@ -35,21 +38,24 @@ function gameOver() {
     setTimeout(() => {
         animationMoveIn();
     }, 1000)
+    setResults();
 }
 
 function animationMoveOut() {
-    let main = document.querySelector(".main"),
+    let container = document.querySelector(".container"),
         startButton = document.querySelector(".start-button");
-    main.classList.remove("animate__fadeInLeftBig");
-    main.classList.add("animate__fadeOutLeftBig");
+    container.classList.remove("animate__fadeInLeftBig");
+    container.classList.add("animate__fadeOutLeftBig");
     startButton.classList.remove("animate__fadeInUpBig");
     startButton.classList.add("animate__fadeOutUpBig");
 }
 
 function animationMoveIn() {
-    let main2 = document.querySelector(".main2");
-    main2.style.display = "block";
-    main2.classList.add("animate__fadeInRightBig");
+    let container2 = document.querySelector(".container2"),
+        container = document.querySelector(".container");
+    container2.style.display = "block";
+    container.style.display = "none";
+    container2.classList.add("animate__fadeInRightBig");
 }
 
 function initCrossHair() {
@@ -64,14 +70,45 @@ function setRound(round) {
     }
 }
 
+function setResults() {
+    let results = allResults(),
+        points = 0;
+
+    for (const [index, result] of results.entries()) {
+        let actualRow = document.querySelector(`tbody tr:nth-child(${index + 1})`),
+            firstCell = actualRow.querySelector('td:nth-child(1)'),
+            actualPoint = parseInt(result[2]);
+        if (actualPoint !== 3) {
+            actualPoint /= 1000;
+            points += actualPoint;
+        } else {
+            points += actualPoint
+        }
+        firstCell.innerHTML = "<td>" + (result[0]) + "</td>";
+        let secondCell = actualRow.querySelector('td:nth-child(2)');
+        secondCell.innerHTML = "<td>" + (result[1]) + "</td>";
+        let thirdCell = actualRow.querySelector('td:nth-child(3)');
+        thirdCell.innerHTML = "<td>" + actualPoint.toString() + "</td>";
+    }
+    let lastRow = document.querySelector(`tbody tr:last-child`),
+        firstCell = lastRow.querySelector('td:nth-child(1)'),
+        lastCell = lastRow.querySelector('td:nth-child(3)');
+    firstCell.innerHTML = "<td>Points</td>";
+    lastCell.innerHTML = "<td>" + points.toFixed(3).toString() + "</td>";
+
+}
+
 function startGame() {
     let cells = document.getElementsByClassName("col"),
-        round = 0,
-        points = 0;
+        round = 0;
+    localStorage.clear();
     const interval = setInterval(function () {
-        if (round === 1) {
+        if (round === 9) {
             clearInterval(interval);
-            gameOver();
+            setTimeout(() => {
+                gameOver();
+            }, 1500)
+
         }
         setRound(round);
         const targetCell = document.querySelector(".target");
@@ -81,53 +118,30 @@ function startGame() {
         let randomNum = (Math.random() * cells.length) | 0;
         cells[randomNum].classList.add("target-bg", "target");
         // start timer
+        let showTime = Date.now();
+        sessionStorage.setItem("showTime", showTime);
         setTimeout(() => {
             cells[randomNum].classList.remove("target-bg");
-        }, 500)
+        }, 250)
         round += 1;
-    }, 3500)
+    }, 1500)
 }
 
-let score = {
-    round1: {
-        score: 0,
-        time: 3
-    },
-    round2: {
-        score: 0,
-        time: 3
-    },
-    round3: {
-        score: 0,
-        time: 3
-    },
-    round4: {
-        score: 0,
-        time: 3
-    },
-    round5: {
-        score: 0,
-        time: 3
-    },
-    round6: {
-        score: 0,
-        time: 3
-    },
-    round7: {
-        score: 0,
-        time: 3
-    },
-    round8: {
-        score: 0,
-        time: 3
-    },
-    round9: {
-        score: 0,
-        time: 3
-    },
-    round10: {
-        score: 0,
-        time: 3
-    },
+function allResults() {
 
+    let results = [],
+        counter = 0;
+    for (let index = 0; index < 10; index++) {
+        let key = "Round" + index.toString(),
+            tempArray = [];
+        if (localStorage.hasOwnProperty(key)) {
+            tempArray.push(counter + 1, "Hit", localStorage.getItem(key))
+        } else {
+            tempArray.push(counter + 1, "Miss", "3")
+        }
+        counter += 1;
+        results.push(tempArray);
+    }
+    // results.push("All hits : " + counter.toString());
+    return results;
 }
